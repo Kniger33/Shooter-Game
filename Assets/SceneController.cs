31c08837c0abf91f9990026f8d1ade2714bc5783
Marmax <marmax199@gmail.com>
2018-02-13 19:24:53 +0300

    v0.0.2

---
+++ Assets/SceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+	// Создание связи с шаблоном
+	[SerializeField] private GameObject enemyPrefab;
+	private GameObject _enemy;
+	private float angle;
+
+	void Update () {
+		// Спавн врагов по условию
+		if (_enemy == null) {
+			_enemy = Instantiate (enemyPrefab) as GameObject;
+			_enemy.transform.transform.position = new Vector3 (0, 1, 0);
+			angle = Random.Range (0, 360);
+			_enemy.transform.Rotate (0, angle, 0);
+		}
+	}
+}
---
+++ Assets/WanderingAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour {
+	// Определяем скорость и расстояние реакции
+	public float speed = 3.0f;
+	public float obstacleRange = 5.0f;
+	private bool _alive = true;
+
+	// Определяем ссылку на снаряд
+	[SerializeField] private GameObject fireballPrefab;
+	private GameObject _fireball;
+
+	void Update () {
+		if (_alive) {
+			// Описываем движение
+			transform.Translate (0, 0, speed * Time.deltaTime);
+
+			// Описываем движение луча
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+
+			// Бросаем луч
+			if (Physics.SphereCast (ray, 0.75f, out hit)) {
+				GameObject hitObject = hit.transform.gameObject;
+
+				// Проверка попадания луча в игрока
+				if (hitObject.GetComponent<PlayerCharacter> ()) {
+					if (_fireball == null) {
+						_fireball = Instantiate (fireballPrefab) as GameObject;
+						// Создаем и направляем снаряд
+						_fireball.transform.position = transform.TransformPoint (Vector3.forward * 1.5f);
+						_fireball.transform.rotation = transform.rotation;
+					}
+				} 
+				// Проверка препятствия и избежание столкновения
+				else if (hit.distance < obstacleRange) {
+					float angle = Random.Range (-110, 110);
+					transform.Rotate (0, angle, 0);
+				}
+			}
+		}
+	}
+
+	public void SetAlive(bool alive) {
+		_alive = alive;
+	}
+}

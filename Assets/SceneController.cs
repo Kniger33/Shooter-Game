36ac68e713af31f8b74083d639d6fa78fb5eb0b6
Marmax <marmax199@gmail.com>
2018-02-15 20:09:12 +0300

    v0.0.3

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -3,18 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneController : MonoBehaviour {
-	// Создание связи с шаблоном
-	[SerializeField] private GameObject enemyPrefab;
-	private GameObject _enemy;
-	private float angle;
 
-	void Update () {
-		// Спавн врагов по условию
-		if (_enemy == null) {
-			_enemy = Instantiate (enemyPrefab) as GameObject;
-			_enemy.transform.transform.position = new Vector3 (0, 1, 0);
-			angle = Random.Range (0, 360);
-			_enemy.transform.Rotate (0, angle, 0);
+	public void ActiveEnemy (bool active) {
+		if (active) {
+			Debug.Log ("1");
 		}
 	}
 }
---
+++ Assets/SpawnController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+	public Transform[] spawnLocations;
+	[SerializeField] private GameObject enemyPrefab;
+	private GameObject _enemy;
+
+	public int enemyCount = 5;
+	private int _count = 0;
+		
+	private float _angle;
+	private int _countLocations;
+
+	void Start () {
+		_countLocations = spawnLocations.Length;
+		for (int i = 0; i < _countLocations; i++) {
+			Spawn (spawnLocations [i]);
+		}
+	}
+		
+	void Update () {
+		SceneController behavior = GetComponentInParent<SceneController> ();
+		if (_enemy == null) {
+			Debug.Log ("null");
+			behavior.ActiveEnemy (false);
+		} else {
+			Debug.Log ("Yes");
+			behavior.ActiveEnemy (true);
+		}
+	}
+
+	void Spawn (Transform spawnLocations) {
+		_count = 0;
+		while (_count < enemyCount) {
+			_enemy = Instantiate (enemyPrefab) as GameObject;
+			_enemy.transform.transform.position = spawnLocations.transform.position;
+			_angle = Random.Range (0, 360);
+			_enemy.transform.Rotate (0, _angle, 0);
+			_count += 1;
+		}
+	}
+}
